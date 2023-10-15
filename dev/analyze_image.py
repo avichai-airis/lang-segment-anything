ad7e7f931176bb088be56fdb6fa96ba1fa61697c
Avichai <avichai@airis-labs.com>
2023-10-15 14:11:03 +0000

    inference now gets the model and pil image

--- dev/analyze_image.py
+++ dev/analyze_image.py
@@ -7,6 +7,7 @@ from io import BytesIO
 from lang_sam import LangSAM
 import time
 from dev.drawing import display_image_with_boxes
+from dev.postprocessing import crop_image
 
 def detect_bb_on_image(model: LangSAM, image: Image,text_prompt: str,  box_threshold=0.3, text_threshold=0.25, show_run_time=False):
     if show_run_time:
@@ -18,12 +19,16 @@ def detect_bb_on_image(model: LangSAM, image: Image,text_prompt: str,  box_thres
         end = time.time()
         print(f"Time: {(end - start):.2f}s")
     return boxes, logits, phrases
-def run_inference( image_path: str,text_prompt: str,  box_threshold=0.3, text_threshold=0.25, show_run_time=False):
-    model = LangSAM()
-    image = Image.open(image_path).convert("RGB")
+
+def run_inference( image: Image,text_prompt: str,model: LangSAM,  box_threshold=0.3, text_threshold=0.25, show_run_time=False):
     boxes, logits, phrases = detect_bb_on_image(model, image, text_prompt, box_threshold=box_threshold, text_threshold=text_threshold, show_run_time=show_run_time)
     display_image_with_boxes(image, boxes, logits)
+    return boxes, logits, phrases
+   
 
 if __name__ == '__main__':
     image_path =  "data/crop_building.png"
-    run_inference(image_path, "buildings")
+    model = LangSAM()
+    text_prompt = "buildings"
+    image = Image.open(image_path).convert("RGB")
+    run_inference(image, text_prompt, model)
